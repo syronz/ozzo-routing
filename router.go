38ef5afd2fee601e72e61157871af721c2cef5e8
Diako <sabina.diako@gmail.com>
2019-04-03 16:32:16 +0300

    Fix CORS plicy problem

--- router.go
+++ router.go
@@ -208,6 +208,8 @@ func MethodNotAllowedHandler(c *Context) error {
 		i++
 	}
 	sort.Strings(ms)
+	c.Response.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Response.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization")
 	c.Response.Header().Set("Allow", strings.Join(ms, ", "))
 	if c.Request.Method != "OPTIONS" {
 		c.Response.WriteHeader(http.StatusMethodNotAllowed)
